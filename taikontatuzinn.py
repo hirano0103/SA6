75e3ee45028e8ef6faf2a816aad71d3f63a035a6
そうたろう <sotarob190@gmail.com>
2025-12-07 00:36:15 +0900

    Update taikontatuzinn.py

--- taikontatuzinn.py
+++ taikontatuzinn.py
@@ -140,7 +140,7 @@ class Rnote:
 def spawn_note():
     if random.random() < 0.02:
         notes.append(Note(random.choice(["don", "ka"])))
-        music = [72][16]
+        music = 
  [       
 [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6],
 [1,2,3,4,3,2,1,0,0,1,2,3,4,3,2,1],
