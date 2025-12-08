f3d0e0996ccb3759edfd8e3be4d4e6a832344038
kaede-warm <nkanoiro@gmail.com>
2025-12-08 16:10:10 +0900

    Update taikontatuzinn.py

--- taikontatuzinn.py
+++ taikontatuzinn.py
@@ -140,8 +140,7 @@ class Rnote:
 def spawn_note():
     if random.random() < 0.02:
         notes.append(Note(random.choice(["don", "ka"])))
-        music = 
- [       
+        music = [       
 [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6],
 [1,2,3,4,3,2,1,0,0,1,2,3,4,3,2,1],
 
