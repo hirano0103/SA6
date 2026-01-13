91e01317418e38968ac5a33da1933bb046f85794
そうたろう <sotarob190@gmail.com>
2026-01-13 23:50:34 +0900

    taikontatuzinn.py

--- taikontatuzinn.py
+++ taikontatuzinn.py
@@ -5,14 +5,6 @@ import time
 
 pygame.init()
 
-# --- 音楽再生設定 ---
-pygame.mixer.init()
-pygame.mixer.music.load("Whiteberry.mp3")  # your_song.mp3 を置き換えてください
-pygame.mixer.music.play()
-
-
-
-
 # --- 画面設定 ---
 WIDTH, HEIGHT = 700, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,14 +15,15 @@ clock = pygame.time.Clock()
 # --- 色 ---
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (220, 50, 50)      
+RED = (220, 50, 50) 
 BLUE = (50, 80, 220)
 GOLD = (255, 215, 0)
 
 # --- ノーツ設定 ---
-NOTE_SPEED = 4
+NOTE_SPEED = 5
 NOTE_SIZE = 30
 HIT_POSITION = 75   # 判定位置
+
 
 REMOVE_SPEED=15
 
@@ -50,6 +43,12 @@ img0 = pygame.transform.scale(img0, (IMAGE_SIZE, IMAGE_SIZE))
 img1 = pygame.transform.scale(img1, (IMAGE_SIZE, IMAGE_SIZE))
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
+don_sound = pygame.mixer.Sound("don.wav")
+ka_sound = pygame.mixer.Sound("ka.wav")
+
+dontyan=-1
+
+hit_type="ka"
 
 notes = []
 rnotes = []
@@ -64,94 +63,18 @@ font = pygame.font.SysFont("msgothic", 40)
 title_font = pygame.font.SysFont("msgothic", 80)
 button_font = pygame.font.SysFont("msgothic", 50)
 
-# --- タイトル画面 ---
-def title_screen():
-    while True:
-        screen.blit(background, (0, 0))
+NOTE_SPEED = 5
+time_span=6 #<10
+music_s_time=60
 
-        # タイトル文字
-        title_text = title_font.render("太鼓の達人", True, GOLD)
-        screen.blit(title_text, (WIDTH//2 - title_text.get_width()//2, 175))
+fx=15
+fy=29
+time_count=0
 
-        # スタートボタン
-        button_rect = pygame.Rect(WIDTH//2 - 80, 300, 150, 80)
-        pygame.draw.rect(screen, WHITE, button_rect)
-        start_text = button_font.render("START", True, BLACK)
-        screen.blit(start_text, (
-            button_rect.centerx - start_text.get_width()//2,
-            button_rect.centery - start_text.get_height()//2))
-
-        pygame.display.update()
-
-        # クリック待ち
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if button_rect.collidepoint(event.pos):
-                    return  # ゲーム開始
-
-
-# --- ノーツクラス ---
-class Note:
-    def __init__(self, type_):
-        self.x = WIDTH
-        self.type = type_
-        self.color = RED if type_ == "don" else BLUE
-
-    def move(self):
-        self.x -= NOTE_SPEED
-        pygame.draw.circle(screen, self.color, (int(self.x), HEIGHT // 2), NOTE_SIZE)
-        pygame.draw.circle(screen, WHITE, (int(self.x), HEIGHT // 2), NOTE_SIZE,3)
-
-    def is_miss(self):
-        return self.x < 30
-
-    def check_hit(self, type_):
-        if self.type == type_ and abs(self.x - HIT_POSITION) < 20:
-            global score, hits, combo, f_img
-            score += (100+combo*10)
-            timing = abs(self.x - HIT_POSITION)
-            if timing < 10:
-                grade = "良"
-                score+=200
-                combo+=1
-                f_img*=-1
-            else:
-                grade = "可"
-                score+=100
-                combo+=1
-                f_img*=-1
-            #else:
-                #grade = "不"
-                #combo=0
-            hits.append((grade, HIT_POSITION-20, HEIGHT//2 - 100, time.time() + 0.3))  # 1秒表示
-            return True
-        return False
+music = [
     
-class Rnote:
-    def __init__(self,type_,x):
-        self.x = x
-        self.y=HEIGHT//2
-        self.type = type_
-        self.color = RED if type_ == "don" else BLUE
-
-    def move(self):
-        self.x +=REMOVE_SPEED+4
-        self.y-=REMOVE_SPEED
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y) ), NOTE_SIZE)
-        pygame.draw.circle(screen, WHITE, (int(self.x), int(self.y)), NOTE_SIZE,3)
-
-# --- 譜面生成 ---
-def spawn_note():
-    if random.random() < 0.02:
-        notes.append(Note(random.choice(["don", "ka"])))
-        music = [       
-[1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6],
-[1,2,3,4,3,2,1,0,0,1,2,3,4,3,2,1],
-
+    
+    
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -190,14 +113,18 @@ def spawn_note():
 [1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 
+[1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],#
+[1,0,0,0,1,0,0,0,2,0,0,6,1,0,1,0],
+[1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],
+[0,0,2,0,0,6,5,2,0,0,0,0,0,0,0,0],
 [1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],
 [1,0,0,0,1,0,0,0,2,0,0,0,1,0,1,0],
-[1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0],
-[1,0,0,0,1,0,0,0,2,0,0,0,1,0,1,0],
-[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+[1,0,0,0,5,0,0,0,1,0,0,0,0,0,0,0],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+
 [1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0],
 [1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0],
 [1,0,0,0,1,0,0,0,2,0,0,0,0,0,0,0],
@@ -228,12 +155,122 @@ def spawn_note():
 [1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0],
 [1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1]
 ]
 
+# --- タイトル画面 ---
+def title_screen():
+    while True:
+        screen.blit(background, (0, 0))
+
+        # タイトル文字
+        title_text = title_font.render("太鼓の達人", True, GOLD)
+        screen.blit(title_text, (WIDTH//2 - title_text.get_width()//2, 175))
+
+        # スタートボタン
+        button_rect = pygame.Rect(WIDTH//2 - 80, 300, 150, 80)
+        pygame.draw.rect(screen, WHITE, button_rect)
+        start_text = button_font.render("START", True, BLACK)
+        screen.blit(start_text, (
+            button_rect.centerx - start_text.get_width()//2,
+            button_rect.centery - start_text.get_height()//2))
+
+        pygame.display.update()
+
+
+        # クリック待ち
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect.collidepoint(event.pos):
+                    return  # ゲーム開始
+
+
+# --- ノーツクラス ---
+class Note:
+    def __init__(self, type_):
+        self.x = WIDTH
+        self.type = type_
+        self.color = RED if type_ == "don" else BLUE
+
+    def move(self):
+        self.x -= NOTE_SPEED
+        pygame.draw.circle(screen, self.color, (int(self.x), HEIGHT // 2), NOTE_SIZE)
+        pygame.draw.circle(screen, WHITE, (int(self.x), HEIGHT // 2), NOTE_SIZE,3)
+
+    def is_miss(self):
+        return self.x < 30
+
+    def check_hit(self, type_):
+        if self.type == type_ and abs(self.x - HIT_POSITION) < 20:
+            global score, hits, combo, f_img
+            score += (100+combo*10)
+            timing = abs(self.x - HIT_POSITION)
+            if timing < 15:
+                grade = "良"
+                score+=200
+                combo+=1
+                f_img*=-1
+            else:
+                grade = "可"
+                score+=100
+                combo+=1
+                f_img*=-1
+            #else:
+                #grade = "不"
+                #combo=0
+            hits.append((grade, HIT_POSITION-20, HEIGHT//2 - 100, time.time() + 0.3))  # 1秒表示
+            return True
+        return False
+    
+class Rnote:
+    def __init__(self,type_,x):
+        self.x = x
+        self.y=HEIGHT//2
+        self.type = type_
+        self.color = RED if type_ == "don" else BLUE
+
+    def move(self):
+        self.x +=REMOVE_SPEED+4
+        self.y-=REMOVE_SPEED
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y) ), NOTE_SIZE)
+        pygame.draw.circle(screen, WHITE, (int(self.x), int(self.y)), NOTE_SIZE,3)
+
+
+
+# --- 譜面生成 ---############################
+def spawn_note():
+    global time_count,fx,fy
+    time_count+=1
+    if time_count%time_span == 0 and music[fy][fx] != -1:
+        if music[fy][fx] == 1:
+            notes.append(Note("don"))
+        elif music[fy][fx] == 2:
+            notes.append(Note("ka"))
+        fx+=1
+        if fx == 16:
+            fx=0
+            fy+=1
+    if music[fy][fx] == 4:
+        music[fy][fx] = 0
+        fx-=3
+    if music[fy][fx] == 5:
+        music[fy][fx] = 0
+        fx-=2
+    if music[fy][fx] == 6:
+        music[fy][fx] = 0
+        fx-=1
 
 # ====== 追加：ゲーム開始前にタイトル画面を呼び出す ======
 title_screen()
+
+pygame.mixer.music.load("summer.mp3") 
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.play(loops=0, start=music_s_time)  # 5秒地点から再生
+
 
 # --- メインループ ---
 running = True
@@ -251,7 +288,13 @@ while running:
 
         # マウス入力
         if event.type == pygame.MOUSEBUTTONDOWN:
-            hit_type = "don" if event.button == 1 else "ka"
+            if event.button == 1:
+                hit_type="don"
+                don_sound.play()
+
+            else:
+                hit_type="ka"
+                ka_sound.play()
             remove_list = []
             for note in notes:
                 if note.check_hit(hit_type):
@@ -267,8 +310,12 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_f:
                 hit_type = "don"
+                don_sound.play()
             elif event.key == pygame.K_j:
                 hit_type = "ka"
+                ka_sound.play()
+            elif event.key == pygame.K_d:
+                dontyan*=-1
             else:
                 hit_type = None
             if hit_type:
@@ -325,14 +372,20 @@ while running:
     combo_text = font.render(f"combo: {combo}", True, WHITE)
     screen.blit(combo_text, (WIDTH-200, HEIGHT-60))
 
-    if f_img == -1:
-        screen.blit(img0, (IMG_X, IMG_Y))
-    elif f_img == 1:
-        screen.blit(img1, (IMG_X, IMG_Y))
+    if dontyan == 1:
+        don_x, don_y = pygame.mouse.get_pos()
+        if f_img == -1:
+            screen.blit(img0, (don_x-75, don_y-75))
+        elif f_img == 1:
+            screen.blit(img1, (don_x-75, don_y-75))
+    else:
+        if f_img == -1:
+            screen.blit(img0, (IMG_X, IMG_Y))
+        elif f_img == 1:
+            screen.blit(img1, (IMG_X, IMG_Y))
 
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
 sys.exit()
-
